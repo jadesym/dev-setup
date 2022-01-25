64ed44f7961679cdd98b05a8aaa4108a037f54d7
Kevin Fu <kevin.fu808@gmail.com>
2022-01-24 22:39:43 -0800

    Adding installation script for docker.

---
+++ README.md
@@ -0,0 +1,1 @@
+# A repository for setting up local development environment.
---
+++ ubuntu/install_docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Setting up some fancy colors for reuse
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Allow docker to be run with non-root user
+sudo groupadd docker --force
+sudo usermod -aG docker $USER
+
+echo -e "\n${GREEN}Finished installing all necessary docker dependencies!${NO_COLOR}"
+
+# Verify everything is working
+# Run the hello-world docker image
+docker --version
+docker-compose --version
+
+# Activate changes to groups
+newgrp docker
